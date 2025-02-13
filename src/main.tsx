f2bf989657026c2bab3b165ea9695dad3c753a0d
Buddy Jiang <dashaomai@gmail.com>
2025-02-13 16:06:31 +0800

    conf: mark the base path configuration out.

--- src/main.tsx
+++ src/main.tsx
@@ -82,6 +82,7 @@ const router = createRouter({
   context: { queryClient },
   defaultPreload: 'intent',
   defaultPreloadStaleTime: 0,
+  basepath: '/',
 })
 
 // Register the router instance for type safety
