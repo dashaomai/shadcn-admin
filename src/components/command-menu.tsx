28c491f0688e07756c331226e885e94f27789b6a
Buddy Jiang <dashaomai@gmail.com>
2025-02-28 18:26:04 +0800

    fix(roles): filter in the command menu and sidebar, to prevent the roles leak in the navigate link.

--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -6,7 +6,9 @@ import {
   IconMoon,
   IconSun,
 } from '@tabler/icons-react'
+import { useRoles } from '@/api/auth'
 import { i18n } from '@/lib/i18n'
+import { rolesCheck } from '@/lib/role'
 import { useSearch } from '@/context/search-context'
 import { useTheme } from '@/context/theme-context'
 import {
@@ -20,11 +22,14 @@ import {
 } from '@/components/ui/command'
 import { sidebarData } from './layout/data/sidebar-data'
 import { ScrollArea } from './ui/scroll-area'
+import { Skeleton } from './ui/skeleton'
 
 export function CommandMenu() {
   const navigate = useNavigate()
   const { setTheme } = useTheme()
   const { open, setOpen } = useSearch()
+
+  const rolesQuery = useRoles()
 
   const runCommand = React.useCallback(
     (command: () => unknown) => {
@@ -34,6 +39,12 @@ export function CommandMenu() {
     [setOpen]
   )
 
+  if (rolesQuery.isFetching) {
+    return <Skeleton className='h-12 w-36 rounded-sm' />
+  } else if (rolesQuery.isError) {
+    return <p>{rolesQuery.error.message}</p>
+  }
+
   return (
     <CommandDialog modal open={open} onOpenChange={setOpen}>
       <CommandInput placeholder={i18n.t('layout.search.input-placeholder')} />
@@ -42,42 +53,46 @@ export function CommandMenu() {
           <CommandEmpty>
             {i18n.t('layout.search.no-results-found')}
           </CommandEmpty>
-          {sidebarData.navGroups.map((group) => (
-            <CommandGroup key={group.title} heading={i18n.t(group.title)}>
-              {group.items.map((navItem, i) => {
-                if (navItem.url)
-                  return (
-                    <CommandItem
-                      key={`${navItem.url}-${i}`}
-                      value={i18n.t(navItem.title)}
-                      onSelect={() => {
-                        runCommand(() => navigate({ to: navItem.url }))
-                      }}
-                    >
-                      <div className='mr-2 flex h-4 w-4 items-center justify-center'>
-                        <IconArrowRightDashed className='size-2 text-muted-foreground/80' />
-                      </div>
-                      {i18n.t(navItem.title)}
-                    </CommandItem>
-                  )
+          {sidebarData.navGroups
+            .filter((group) => rolesCheck(rolesQuery.data, group.roles))
+            .map((group) => (
+              <CommandGroup key={group.title} heading={i18n.t(group.title)}>
+                {group.items
+                  .filter((item) => rolesCheck(rolesQuery.data, item.roles))
+                  .map((navItem, i) => {
+                    if (navItem.url)
+                      return (
+                        <CommandItem
+                          key={`${navItem.url}-${i}`}
+                          value={i18n.t(navItem.title)}
+                          onSelect={() => {
+                            runCommand(() => navigate({ to: navItem.url }))
+                          }}
+                        >
+                          <div className='mr-2 flex h-4 w-4 items-center justify-center'>
+                            <IconArrowRightDashed className='size-2 text-muted-foreground/80' />
+                          </div>
+                          {i18n.t(navItem.title)}
+                        </CommandItem>
+                      )
 
-                return navItem.items?.map((subItem, i) => (
-                  <CommandItem
-                    key={`${subItem.url}-${i}`}
-                    value={i18n.t(subItem.title)}
-                    onSelect={() => {
-                      runCommand(() => navigate({ to: subItem.url }))
-                    }}
-                  >
-                    <div className='mr-2 flex h-4 w-4 items-center justify-center'>
-                      <IconArrowRightDashed className='size-2 text-muted-foreground/80' />
-                    </div>
-                    {i18n.t(subItem.title)}
-                  </CommandItem>
-                ))
-              })}
-            </CommandGroup>
-          ))}
+                    return navItem.items?.map((subItem, i) => (
+                      <CommandItem
+                        key={`${subItem.url}-${i}`}
+                        value={i18n.t(subItem.title)}
+                        onSelect={() => {
+                          runCommand(() => navigate({ to: subItem.url }))
+                        }}
+                      >
+                        <div className='mr-2 flex h-4 w-4 items-center justify-center'>
+                          <IconArrowRightDashed className='size-2 text-muted-foreground/80' />
+                        </div>
+                        {i18n.t(subItem.title)}
+                      </CommandItem>
+                    ))
+                  })}
+              </CommandGroup>
+            ))}
           <CommandSeparator />
           <CommandGroup heading={i18n.t('layout.theme.title')}>
             <CommandItem onSelect={() => runCommand(() => setTheme('light'))}>
--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -1,3 +1,5 @@
+import { useRoles } from '@/api/auth'
+import { rolesCheck } from '@/lib/role'
 import {
   Sidebar,
   SidebarContent,
@@ -8,18 +10,29 @@ import {
 import { NavGroup } from '@/components/layout/nav-group'
 import { NavUser } from '@/components/layout/nav-user'
 import { TeamSwitcher } from '@/components/layout/team-switcher'
+import { Skeleton } from '../ui/skeleton'
 import { sidebarData } from './data/sidebar-data'
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const rolesQuery = useRoles()
+
+  if (rolesQuery.isFetching) {
+    return <Skeleton className='h-12 w-12 rounded-full' />
+  } else if (rolesQuery.isError) {
+    return <p>{rolesQuery.error.message}</p>
+  }
+
   return (
     <Sidebar collapsible='icon' variant='floating' {...props}>
       <SidebarHeader>
         <TeamSwitcher teams={sidebarData.teams} />
       </SidebarHeader>
       <SidebarContent>
-        {sidebarData.navGroups.map((props) => (
-          <NavGroup key={props.title} {...props} />
-        ))}
+        {sidebarData.navGroups
+          .filter((group) => rolesCheck(rolesQuery.data, group.roles))
+          .map((props) => (
+            <NavGroup key={props.title} {...props} />
+          ))}
       </SidebarContent>
       <SidebarFooter>
         <NavUser />
