abe2a3cd448a2ec63c6dd9d17f3b02629ebc7589
Buddy Jiang <dashaomai@gmail.com>
2025-05-07 10:51:47 +0800

    feat(settings): 初步替换了 profile 页面的多语言资源及表单内容。

--- src/features/settings/profile/index.tsx
+++ src/features/settings/profile/index.tsx
@@ -1,11 +1,12 @@
+import { i18n } from '@/lib/i18n'
 import ContentSection from '../components/content-section'
 import ProfileForm from './profile-form'
 
 export default function SettingsProfile() {
   return (
     <ContentSection
-      title='Profile'
-      desc='This is how others will see you on the site.'
+      title={i18n.t('settings.profile.title')}
+      desc={i18n.t('settings.profile.description')}
     >
       <ProfileForm />
     </ContentSection>
--- src/features/settings/profile/profile-form.tsx
+++ src/features/settings/profile/profile-form.tsx
@@ -1,9 +1,3 @@
-import { z } from 'zod'
-import { useFieldArray, useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Link } from '@tanstack/react-router'
-import { cn } from '@/lib/utils'
-import { showSubmittedData } from '@/utils/show-submitted-data'
 import { Button } from '@/components/ui/button'
 import {
   Form,
@@ -15,48 +9,26 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
 import { Textarea } from '@/components/ui/textarea'
+import { i18n } from '@/lib/i18n'
+import { showSubmittedData } from '@/utils/show-submitted-data'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 const profileFormSchema = z.object({
-  username: z
-    .string()
-    .min(2, {
-      message: 'Username must be at least 2 characters.',
-    })
-    .max(30, {
-      message: 'Username must not be longer than 30 characters.',
-    }),
+  nickname: z
+    .string(),
   email: z
-    .string({
-      required_error: 'Please select an email to display.',
-    })
+    .string()
     .email(),
-  bio: z.string().max(160).min(4),
-  urls: z
-    .array(
-      z.object({
-        value: z.string().url({ message: 'Please enter a valid URL.' }),
-      })
-    )
-    .optional(),
+  avatar: z.string().url().optional(),
 })
 
 type ProfileFormValues = z.infer<typeof profileFormSchema>
 
 // This can come from your database or API.
 const defaultValues: Partial<ProfileFormValues> = {
-  bio: 'I own a computer.',
-  urls: [
-    { value: 'https://shadcn.com' },
-    { value: 'http://twitter.com/shadcn' },
-  ],
 }
 
 export default function ProfileForm() {
@@ -64,11 +36,6 @@ export default function ProfileForm() {
     resolver: zodResolver(profileFormSchema),
     defaultValues,
     mode: 'onChange',
-  })
-
-  const { fields, append } = useFieldArray({
-    name: 'urls',
-    control: form.control,
   })
 
   return (
@@ -79,16 +46,15 @@ export default function ProfileForm() {
       >
         <FormField
           control={form.control}
-          name='username'
+          name='nickname'
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>{i18n.t('apps.accounts.properties.profile.nickname.title')}</FormLabel>
               <FormControl>
-                <Input placeholder='shadcn' {...field} />
+                <Input placeholder={i18n.t('apps.accounts.properties.profile.nickname.placeholder')} readOnly {...field} />
               </FormControl>
               <FormDescription>
-                This is your public display name. It can be your real name or a
-                pseudonym. You can only change this once every 30 days.
+                {i18n.t('apps.accounts.properties.profile.nickname.description')}
               </FormDescription>
               <FormMessage />
             </FormItem>
@@ -99,22 +65,12 @@ export default function ProfileForm() {
           name='email'
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Email</FormLabel>
-              <Select onValueChange={field.onChange} defaultValue={field.value}>
-                <FormControl>
-                  <SelectTrigger>
-                    <SelectValue placeholder='Select a verified email to display' />
-                  </SelectTrigger>
-                </FormControl>
-                <SelectContent>
-                  <SelectItem value='m@example.com'>m@example.com</SelectItem>
-                  <SelectItem value='m@google.com'>m@google.com</SelectItem>
-                  <SelectItem value='m@support.com'>m@support.com</SelectItem>
-                </SelectContent>
-              </Select>
+              <FormLabel>{i18n.t('apps.accounts.properties.profile.email.title')}</FormLabel>
+              <FormControl>
+                <Input placeholder={i18n.t('apps.accounts.properties.profile.email.placeholder')} readOnly {...field} />
+              </FormControl>
               <FormDescription>
-                You can manage verified email addresses in your{' '}
-                <Link to='/'>email settings</Link>.
+                {i18n.t('apps.accounts.properties.profile.email.description')}
               </FormDescription>
               <FormMessage />
             </FormItem>
@@ -122,58 +78,26 @@ export default function ProfileForm() {
         />
         <FormField
           control={form.control}
-          name='bio'
+          name='avatar'
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Bio</FormLabel>
+              <FormLabel>{i18n.t('apps.accounts.properties.profile.avatar.title')}</FormLabel>
               <FormControl>
                 <Textarea
-                  placeholder='Tell us a little bit about yourself'
+                  placeholder={i18n.t('apps.accounts.properties.profile.avatar.placeholder')}
                   className='resize-none'
                   {...field}
                 />
               </FormControl>
               <FormDescription>
-                You can <span>@mention</span> other users and organizations to
-                link to them.
+                {i18n.t('apps.accounts.properties.profile.avatar.description')}
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <div>
-          {fields.map((field, index) => (
-            <FormField
-              control={form.control}
-              key={field.id}
-              name={`urls.${index}.value`}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className={cn(index !== 0 && 'sr-only')}>
-                    URLs
-                  </FormLabel>
-                  <FormDescription className={cn(index !== 0 && 'sr-only')}>
-                    Add links to your website, blog, or social media profiles.
-                  </FormDescription>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          ))}
-          <Button
-            type='button'
-            variant='outline'
-            size='sm'
-            className='mt-2'
-            onClick={() => append({ value: '' })}
-          >
-            Add URL
-          </Button>
-        </div>
-        <Button type='submit'>Update profile</Button>
+        
+        <Button type='submit'>{i18n.t('apps.accounts.actions.updateProfile.submit')}</Button>
       </form>
     </Form>
   )
