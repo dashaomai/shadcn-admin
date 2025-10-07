07c00b7d9fe691888414f679e91fc1027af9758c
Buddy Jiang <dashaomai@gmail.com>
2025-10-07 13:28:36 +0800

    feat(anchorSummaries): 补充查询按钮，组合当前的 search parameter 后生成新的链接。

--- src/features/anchor-summaries/components/anchor-summaries-primary-buttons.tsx
+++ src/features/anchor-summaries/components/anchor-summaries-primary-buttons.tsx
@@ -1,14 +1,35 @@
-import { Button } from '@/components/ui/button.tsx'
-import { DownloadIcon } from 'lucide-react'
+import { DownloadIcon, SearchIcon } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
+import { Button } from '@/components/ui/button.tsx'
+import { Link } from '@tanstack/react-router'
+import { useRangeDate } from '@/components/context/range-date-context.tsx'
+import { useGameFilter } from '@/components/context/game-filter-context.tsx'
+import { useAnchorFilter } from '@/components/context/anchor-filter-context.tsx'
+import { Route } from '@/routes/_authenticated/anchorSummaries'
+import { formatToDateOnly } from '@/utils/time.ts'
 
 export function AnchorSummariesPrimaryButtons() {
   const { t } = useTranslation()
 
+  const {begin, end} = useRangeDate()
+  const {values: games} = useGameFilter()
+  const {values: anchors} = useAnchorFilter()
+
   return (
     <div className='flex gap-2'>
-      <Button className='space-x-1' onClick={e => e.preventDefault()}>
-        <span>{t('apps.anchorSummaries.export')}</span>
+      <Link
+        className='space-x-1'
+        from={Route.fullPath}
+        search={prev => ({...prev, begin: formatToDateOnly(begin), end: formatToDateOnly(end), games, anchors})}
+      >
+        <Button>
+          <span>{t('common.query')}</span>
+          <SearchIcon />
+        </Button>
+      </Link>
+
+      <Button className='space-x-1' onClick={(e) => e.preventDefault()}>
+        <span>{t('common.export')}</span>
         <DownloadIcon />
       </Button>
     </div>
